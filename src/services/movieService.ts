6a79e9b380230e755a80de8e36579d80ac44dcbf
Snizhana <snizhanapetrushka@gmail.com>
2026-03-08 13:19:39 +0200

    changes

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -2,6 +2,7 @@ import axios, { type AxiosResponse } from "axios";
 import type { Movie } from "../types/movie";
 
 const BASE_URL = "https://api.themoviedb.org/3";
+const token = import.meta.env.VITE_TMDB_TOKEN;
 
 export const fetchMovies = async (query: string): Promise<Movie[]> => {
   const response: AxiosResponse<{ results: Movie[] }> = await axios.get(
@@ -9,7 +10,7 @@ export const fetchMovies = async (query: string): Promise<Movie[]> => {
     {
       params: { query },
       headers: {
-        Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`,
+        Authorization: `Bearer ${token}`,
       },
     }
   );
